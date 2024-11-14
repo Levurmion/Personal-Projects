91d0373c5ac5af96ea9ac75ab236c1937ca08f4f
Elbert Timothy <elbert.timothy@touchstoneadvisory.co.uk>
2024-11-14 21:05:38 +0000

    first commit?

--- packages/config-eslint/react.js
+++ packages/config-eslint/react.js
@@ -37,6 +37,14 @@ module.exports = {
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        "pascalCase": true,
+        "kebabCase": true
+      }
+    ]
   },
   overrides: [
     {
