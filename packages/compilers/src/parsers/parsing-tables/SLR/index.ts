c550cd932ce5e73fb8c83a7151f8e9ffc9af6206
Elbert Timothy <elbert.timothy@touchstoneadvisory.co.uk>
2024-11-28 23:02:27 +0000

    added README

--- packages/compilers/src/parsers/parsing-tables/SLR/index.ts
+++ packages/compilers/src/parsers/parsing-tables/SLR/index.ts
@@ -1,12 +1,12 @@
 import { augmentGrammar } from "../..";
 import { Language } from "../../language";
 import { Automaton } from "../../automaton";
-import { arithmeticGrammar } from "../../tests/mocks";
+import { arithmeticGrammar, jsonGrammar } from "../../tests/mocks";
 import { SLRParsingTable } from "./SLR-parsing-table";
 
-const augmentedGrammar = augmentGrammar(arithmeticGrammar);
+const augmentedGrammar = augmentGrammar(jsonGrammar);
 const language = new Language(augmentedGrammar);
 const automaton = new Automaton(language);
 const slrParsingTable = new SLRParsingTable(automaton);
 
-console.log(slrParsingTable.ACTION_GOTO(10, "*"));
+// console.log(slrParsingTable.ACTION_GOTO(10, ""));
